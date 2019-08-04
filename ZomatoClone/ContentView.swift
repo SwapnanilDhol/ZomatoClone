86a080930aeacb7047b4bcc5c4476005ffee588a
Swapnanil Dhol <swapnanildhol@gmail.com>
2019-08-04 10:22:48 +0530

    Initial Commit

---
+++ ZomatoClone/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  ZomatoClone
+//
+//  Created by Swapnanil Dhol on 8/4/19.
+//  Copyright © 2019 Swapnanil Dhol. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
